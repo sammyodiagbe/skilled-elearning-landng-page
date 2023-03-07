146c4fbba9afa03603debfa65d57df7d0eb3986b
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2023-03-06 22:28:36 -0600

    working on structure

---
+++ src/components/icon-crypto.js
@@ -0,0 +1,26 @@
+const IconCrypto = () => {
+  return (
+    <svg
+      viewBox="0 0 56 56"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+    >
+      <defs>
+        <linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="a">
+          <stop stopColor="#FF6F48" offset="0%" />
+          <stop stopColor="#F02AA6" offset="100%" />
+        </linearGradient>
+        <path
+          d="M26 28c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2Zm-4-4c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2Zm0 8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2Zm12-8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2Zm-4 8c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2Zm4-4c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2Zm-4-4c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2Zm-4-4c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2Z"
+          id="b"
+        />
+      </defs>
+      <g fill="none" fillRule="evenodd">
+        <circle fill="url(#a)" cx="28" cy="28" r="28" />
+        <use fill="#FFF" xlinkHref="#b" />
+      </g>
+    </svg>
+  );
+};
+
+export default IconCrypto;
